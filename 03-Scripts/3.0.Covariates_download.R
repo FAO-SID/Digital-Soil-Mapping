851fd1f6aa777c2cb4170475192649c341166331
Isabel Luotto <72745296+Isa-Luotto@users.noreply.github.com>
2022-08-24 20:01:15 +0200

    replaced PCA with RFE

--- 03-Scripts/3.0.Covariates_download.R
+++ 03-Scripts/3.0.Covariates_download.R
@@ -99,10 +99,10 @@ for (i in unique(assetname_clim$ID)){
   image <- ee$Image(i) %>%
     ee$Image$clip(region)
   
-  # # Resample to target resolution
-  #   image = image$resample('bilinear')$reproject(
-  #   crs= crs,
-  #   scale= res)
+  # Resample to target resolution
+    image = image$resample('bilinear')$reproject(
+    crs= crs,
+    scale= res)
   
   #Export clipped covariate as raster
   raster <- ee_as_raster(
@@ -131,10 +131,10 @@ for (i in unique(assetname_veg$ID)){
   image <- ee$Image(i) %>%
     ee$Image$clip(region)
   
-  # # Resample to target resolution
-  #   image = image$resample('bilinear')$reproject(
-  #   crs= crs,
-  #   scale= res)
+  # Resample to target resolution
+    image = image$resample('bilinear')$reproject(
+    crs= crs,
+    scale= res)
   
   #Export clipped covariate as raster
   raster <- ee_as_raster(
@@ -186,7 +186,7 @@ for (i in unique(assetname_lc$ID)){
 #Obtain list of terrain attributes
 assetname_ta <-  ee_manage_assetlist(path_asset = "projects/digital-soil-mapping-gsp-fao/assets/OPENLANDMAP")
 
-# Loop over the names of assets to clip and dowload the covariates
+# Loop over the names of assets to clip and download the covariates
 for (i in unique(assetname_ta$ID)){
   
   #Extract filename 
@@ -214,176 +214,5 @@ for (i in unique(assetname_ta$ID)){
   
 }
 
-###########################################################################
-# Export PCs based on Principle Component Analysis ----
-assetname_clim <-  ee_manage_assetlist(path_asset = "projects/digital-soil-mapping-gsp-fao/assets/CHELSA")
-assetname_lc <-  ee_manage_assetlist(path_asset = "projects/digital-soil-mapping-gsp-fao/assets/LANDCOVER")
-assetname_ta <-  ee_manage_assetlist(path_asset = "projects/digital-soil-mapping-gsp-fao/assets/OPENLANDMAP")
-assetname_veg <-  ee_manage_assetlist(path_asset = "projects/digital-soil-mapping-gsp-fao/assets/MODIS")
 
-
-assetname <- rbind(assetname_clim, assetname_lc, assetname_ta,assetname_veg)
-
-# clip images
-for (i in unique(assetname$ID)){
-  #Extract filename 
-  filename <- sub('.*\\/', '', i)
-  
-  #Clip image to the extent of the AOI
-  image <- ee$Image(i) %>%
-    ee$Image$clip(region)
-  
-  assign(filename, image)
-}
-
-
-# Stack bands 
-assetname$names <- sub('.*\\/', '', assetname$ID)
-
-SG <- bio1
-
-assetname <- assetname[assetname$names!= 'bio1',]
-
-for (i in unique(assetname$names)){
-  
-  image <- get(i)
-  SG <-SG$addBands(image)
-}
-
-inputBandNames <- SG$bandNames()$getInfo()
-print(inputBandNames)
-
-dimOne <- length(inputBandNames)
-cat("Number of input bands:", dimOne)
-
-# Calculate scale standardize each band to mean=0, s.d.=1. ----
-scale <- SG$select("b1")$projection()$nominalScale()
-cat("Nominal scale: ", scale$getInfo())
-
-SGmean <- SG$reduceRegion(ee$Reducer$mean(),
-                          geometry = region, scale = scale, bestEffort = TRUE)
-head(SGmean$getInfo(),3) # an example of the means
-
-SGsd <- SG$reduceRegion(ee$Reducer$stdDev(),
-                        geometry = region, scale = scale, bestEffort = TRUE)
-head(SGsd$getInfo(),3)
-
-# saveRDS(as.vector(SGmean$getInfo()), file = "./inputBandMeans.rds")
-# saveRDS(as.vector(SGsd$getInfo()), file = "./inputBandSDs.rds")
-
-SGmean.img <- ee$Image$constant(SGmean$values(inputBandNames))
-SGsd.img <- ee$Image$constant(SGsd$values(inputBandNames))
-
-#Standardize
-SGstd <- SG$subtract(SGmean.img)
-SGstd <- SGstd$divide(SGsd.img)
-
-
-SGstd.minMax <- SGstd$reduceRegion(ee$Reducer$minMax(),
-                                   geometry = region, scale = scale,
-                                   maxPixels = 1e9, bestEffort = TRUE)
-minMaxNames <- names(SGstd.minMax$getInfo())
-minMaxVals <- SGstd.minMax$values()$getInfo()
-# per property min/max
-head(data.frame(property_depth_q=minMaxNames, value=minMaxVals), 12)
-
-# overall min/max
-print(c(min(minMaxVals), max(minMaxVals)) )
-
-# convert image to an array of pixels, for matrix calculations
-# dimensions are N x P; i.e., pixels x bands
-arrays <- SGstd$toArray()
-
-
-# Compute the covariance of the bands within the region.
-covar <- arrays$reduceRegion(
-  ee$Reducer$centeredCovariance(),
-  geometry = region, scale = scale,
-  maxPixels = 1e6,
-  bestEffort = TRUE
-)
-
-
-# Get the covariance result and cast to an array.
-# This represents the band-to-band covariance within the region.
-covarArray <- ee$Array(covar$get('array'))
-# note we know the dimensions from the inputs
-# Perform an eigen analysis and slice apart the values and vectors.
-eigens <- covarArray$eigen()
-# the first item of each slice (PC) is the corresponding eigenvalue
-# the remaining items are the eigenvalues (rotations) for that PC
-
-# by removing the first axis (eigenvalues) and converting to a vector
-# array$slice(axis=0, start=0, end=null, step=1)
-# here we only have one axis, indexed by the PC
-eigenValues <- eigens$slice(1L, 0L, 1L)
-cat('Eigenvalues:')
-## Eigenvalues:
-print(eigenValues.vect <- unlist(eigenValues$getInfo()))
-
-# compute and show proportional eigenvalues
-eigenValuesProportion <-
-  round(100*(eigenValues.vect/sum(eigenValues.vect)),2)
-cat('PCs percent of variance explained:')
-## PCs percent of variance explained:
-print(eigenValuesProportion)
-
-plot(eigenValuesProportion, type="h", xlab="PC",
-     ylab="% of variance explained",
-     main="Standardized PCA")
-
-evsum <- cumsum(eigenValuesProportion)
-cat('PCs percent of variance explained, cumulative sum:')
-## PCs percent of variance explained, cumulative sum:
-print(round(evsum,1))
-
-cat(npc95 <- which(evsum > 95)[1],
-    "PCs are needed to explain 95% of the variance")
-
-#saveRDS(eigenValues.vect, file = "./eigenValuesVector.rds")
-
-
-# The eigenvectors (rotations); this is a PxP matrix with eigenvectors in rows.
-eigenVectors <- eigens$slice(1L, 1L)
-# show the first few rotations
-cat('Eigenvectors 1--3:')
-## Eigenvectors 1--3:
-print(data.frame(band=inputBandNames,
-                 rotation1 = eigenVectors$getInfo()[[1]],
-                 rotation2 = eigenVectors$getInfo()[[2]],
-                 rotation3 = eigenVectors$getInfo()[[3]]
-))
-
-#export the eigenvectors ----
-eVm <- matrix(unlist(eigenVectors$getInfo()), byrow = TRUE, nrow = dimOne)
-#saveRDS(eVm, file = "./eigenvectorMatrix.rds")
-
-arrayImage <- arrays$toArray(1L)
-PCsMatrix <- ee$Image(eigenVectors)$matrixMultiply(arrayImage)
-
-# arrayProject: "Projects the array in each pixel to a lower dimensional #
-#space by specifying the axes to retain"
-# arrayFlatten: "Converts a single band image of equal-shape
-# multidimensional pixels
-# to an image of scalar pixels,
-# with one band for each element of the array."
-PCbandNames <- as.list(paste0('PC', seq(1L:dimOne)))
-PCs <- PCsMatrix$arrayProject(list(0L))$arrayFlatten(
-  list(PCbandNames)
-)
-
-PCs95 <- PCs$select(0L:(npc95-1L)) # indexing starts from 0
-PCs95$bandNames()$getInfo()
-
-# Export PCs as raster stack ----
-PCAs_covs <- ee_as_raster(
-  image = PCs95,
-  scale= res,
-  region = region,
-  via = "drive"
-)
-
-
-
-writeRaster(PCAs_covs, paste0(output_dir, 'PCAs_covs.tif'), overwrite=T)
 
