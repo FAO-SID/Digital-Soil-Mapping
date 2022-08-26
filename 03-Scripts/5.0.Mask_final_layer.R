1973492339b73ac39a315afd27491376f535040d
Marcos Angelini <angelini75@gmail.com>
2022-08-26 17:29:28 -0300

    add zonal stat and plots

--- 03-Scripts/5.0.Mask_final_layer.R
+++ 03-Scripts/5.0.Mask_final_layer.R
@@ -24,9 +24,9 @@ wd <- 'C:/Users/luottoi/Documents/GitHub/Digital-Soil-Mapping'
 # Area of interest
 AOI <- '01-Data/MKD.shp'
 # GEE Resolution (CRS defined based on the first TerraClimate layer WGS84 )
-res = 1000
+res = 250
 
-soilatt <- 'OCS'
+soilatt <- 'ocs_0_30'
 
 #
 #
@@ -34,10 +34,12 @@ soilatt <- 'OCS'
 
 
 # Load libraries ----
-library(data.table)
+library(tidyverse)
 library(terra)
 library(rgee)
 library(sf)
+library(tmap)
+
 
 # Set working directory ----
 setwd(wd)
@@ -56,7 +58,7 @@ ee_Initialize()
 
 #Convert shp to gee geometry
 region <- sf_as_ee(AOI)
-region = region$geometry()
+region <- region$geometry()
 
 
 # Water bodies mask  ----
@@ -70,9 +72,9 @@ image1 <- ee$Image("JRC/GSW1_0/GlobalSurfaceWater") %>%
   ee$Image$toDouble()
 
 
-proj = image1$projection()$getInfo()
+proj <- image1$projection()$getInfo()
 
-crs =proj$crs
+crs <- proj$crs
 
 image1 = image1$resample('bilinear')$reproject(
   crs= crs,
@@ -88,20 +90,66 @@ wtbs <- ee_as_raster(
 )
 
 wtbs <-rast(wtbs)
-wtbs[wtbs$occurrence <=90] <-0
-wtbs[wtbs$occurrence >90] <-1
-wtbs[wtbs$occurrence ==0] <-NA
+plot(wtbs)
+wtbs[wtbs$occurrence <=90] <-NaN
+wtbs[wtbs$occurrence >=90] <-1
 
 
 #Upload final maps and mask out waterbodies
-mean <- rast(paste0(wd,'/02-Outputs/maps/',soilatt,'_QRF.tif'))
-sd <- rast(paste0(wd,'/02-Outputs/maps/',soilatt,'_QRF_SD.tif'))
+pred_mean <- rast(paste0('02-Outputs/maps/',soilatt,'_QRF.tif'))
+pred_sd <- rast(paste0('02-Outputs/maps/',soilatt,'_QRF_SD.tif'))
 
 
-mean <- mask(mean, wtbs, inverse=T)
-sd <- mask(sd, wtbs, inverse=T)
+pred_mean <- mask(pred_mean, wtbs, inverse=T)
+pred_sd <- mask(pred_sd, wtbs, inverse=T)
 
+plot(pred_mean)
+plot(pred_sd)
 
 #Export masked layers
-writeRaster(mean, paste0(wd,'/02-Outputs/maps/',soilatt,'_QRF_mask.tif'), overwrite=T)
-writeRaster(sd, paste0(wd,'/02-Outputs/maps/',soilatt,'_QRF_SD_mask.tif'), overwrite=T)
+writeRaster(pred_mean, paste0('02-Outputs/maps/',soilatt,'_QRF_mask.tif'), overwrite=T)
+writeRaster(pred_sd, paste0('02-Outputs/maps/',soilatt,'_QRF_SD_mask.tif'), overwrite=T)
+
+# 3 - Zonal statistics =========================================================
+
+zones <- vect('01-Data/soil map/SoilTypes.shp')
+plot(zones, "Symbol")
+
+x <- terra::extract(pred_mean, zones)
+zones$ocs_0_30 <- x$lyr1
+
+plot(zones, "ocs_0_30")
+
+writeVector(zones, '02-Outputs/maps/zones.shp', overwrite = TRUE)
+
+# 3.1 - Plot zonal stat
+
+(zones <- as_tibble(zones))
+
+unique(select(zones, FAO, Symbol))
+
+ggplot(zones, aes(x = Symbol, y = ocs_0_30, fill = Symbol)) + 
+  geom_boxplot() + theme(legend.position = "") +
+  ylab("Organic Carbon Stock 0-30cm tn/ha") + 
+  xlab("Cartographic Symbol") + 
+  coord_flip()
+
+# Plot maps 
+pred <- c(pred_mean, pred_sd)
+names(pred) <- c("predicted mean", "predicted sd")
+tm_shape(pred) +
+  tm_raster(title = "OCS 0-30cm \ntn/ha", style = "cont") +
+  tm_layout(legend.outside=TRUE, legend.just = c("left", "top"), 
+            legend.width=-0.17, legend.height=-0.3,
+            inner.margins = c(0,0,0,0)) 
+  
+tm_shape(pred_mean, style = "natural") +
+  tm_raster(title = "OCS 0-30cm \ntn/ha", style = "cont") +
+  tm_compass(type = "rose", size = 1.5, color.dark = "#656565", 
+             position = c("RIGHT", "TOP")) +
+  tm_scale_bar(breaks = c(0,50), color.dark = "#656565",
+               position = c("RIGHT", "BOTTOM")) +
+  tm_layout(legend.outside=TRUE, legend.just = c("left", "top"), 
+            legend.width=-0.17, legend.height=-0.3,
+            inner.margins = c(0,0,0,0)) 
+
