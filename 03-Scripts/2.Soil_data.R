749379ce3784baf3f530102a089354d7723675be
Marcos Angelini <angelini75@gmail.com>
2022-10-03 17:51:09 -0300

    last update

--- 03-Scripts/2.Soil_data.R
+++ 03-Scripts/2.Soil_data.R
@@ -37,10 +37,8 @@ library(tidyverse) # for data management and reshaping
 library(readxl) # for importing excel files
 library(mapview) # for seeing the profiles in a map
 library(sf) # to manage spatial data (shp vectors) 
-library(aqp) # for soil profile data
 # install.packages("devtools") 
 # devtools::install_bitbucket("brendo1001/ithir/pkg") #install ithir package
-library(ithir) # for horizon harmonization
 
 
 # 2 - Import national data =====================================================
@@ -53,7 +51,7 @@ dxy <- read_excel("01-Data/data_with_coord.xlsx", sheet = 1)
 dxy <- select(dxy,LabID, x=Long, y=Lat, p=P_ppm)
 dxy <- dxy %>% na.omit()
 # Import data without coordinates
-darea <- read_excel("01-Data/data_without_coord.xls", sheet = "p_bray")
+darea <- readxl::read_excel("01-Data/Buenos_Aires_sur_P_Bray_MA.xls")
 
 # 3 - select useful columns ====================================================
 # 4 - Quality check ============================================================
@@ -70,68 +68,70 @@ dxy <- filter(dxy, p < 100)
 hist(dxy$p)
 summary(dxy)
 
+write_csv(dxy, "01-Data/data_with_coord.csv")
 ## data without coordinates
 
-darea <- darea %>% 
-  select(district=Partido, code, mean, x0=min, x0.25:x1) %>% 
-  as.data.frame()
+# library(splines2)
+# estimate <- function(x){
+#   new_x <- predict(fit_spline, data.frame(cum_probs = c(x)))
+#   return(new_x)
+# }
+# 
+# quants_df <- select(darea, x0:x1)
+# district <- darea$code
+# x <- NULL
+# cum_probs <- seq(0.0, 1, 0.25)
+# for (i in 1:nrow(quants_df)) {
+#   quants <- t(quants_df[i,])
+#   row.names(quants) <- NULL
+#   df_quants <- data.frame(cum_probs, (quants))
+#   names(df_quants)[2] <- "quants"
+#   fit_spline <- lm(quants ~ bSpline(cum_probs, df = 5), df_quants)
+#   df_quants$fit_spline <- predict(fit_spline, df_quants)
+#   y <-(estimate(runif(200, 0, 1)))
+#   x <- rbind(x, data.frame(district[i], p=y))
+# }
+# names(x) <- c("district", "p")
 
-library(splines2)
-estimate <- function(x){
-  new_x <- predict(fit_spline, data.frame(cum_probs = c(x)))
-  return(new_x)
-}
-
-quants_df <- select(darea, x0:x1)
-district <- darea$code
-x <- NULL
-cum_probs <- seq(0.0, 1, 0.25)
-for (i in 1:nrow(quants_df)) {
-  quants <- t(quants_df[i,])
-  row.names(quants) <- NULL
-  df_quants <- data.frame(cum_probs, (quants))
-  names(df_quants)[2] <- "quants"
-  fit_spline <- lm(quants ~ bSpline(cum_probs, df = 5), df_quants)
-  df_quants$fit_spline <- predict(fit_spline, df_quants)
-  y <-(estimate(runif(200, 0, 1)))
-  x <- rbind(x, data.frame(district[i], p=y))
-}
-names(x) <- c("district", "p")
-write_csv(x, "01-Data/sim_data_no_coord.csv")
 
 library(terra)
-aoi <- vect("01-Data/buenos_aires.shp")
-crop1 <- rast("01-Data/land cover/mnc-invierno-2020.tif")
-crop2 <- rast("01-Data/land cover/mnc-verano-2021.tif")
-covs <- rast("01-Data/covs/bio1.tif")
 
-crop1 <- project(crop1, covs)
-crop2 <- project(crop2, covs)
+aoi <- vect("01-Data/AOI_Arg.shp")
 
-crop1[crop1$`mnc-invierno-2020`==20] <- 0
-crop1[crop1$`mnc-invierno-2020`!= 0] <- 1
-plot(crop1)
+darea <- darea %>% filter(stratum %in% aoi$stratum)
+write_csv(darea, "01-Data/sim_data_no_coord.csv")
+# crop1 <- rast("01-Data/land cover/mnc-invierno-2020.tif")
+# crop2 <- rast("01-Data/land cover/mnc-verano-2021.tif")
+# covs <- rast("01-Data/covs/bio1.tif")
+# 
+# crop1 <- project(crop1, covs)
+# crop2 <- project(crop2, covs)
+# 
+# crop1[crop1$`mnc-invierno-2020`==20] <- 0
+# crop1[crop1$`mnc-invierno-2020`!= 0] <- 1
+# plot(crop1)
+# 
+# crop2[crop2$`mnc-verano-2021`==22] <- 0
+# crop2[crop2$`mnc-verano-2021`!=0] <- 1
+# plot(crop2)
+# crops <- crop1+crop2
+# crops[crops$`mnc-invierno-2020`==2] <- 1
+# names(crops) <- "crops"
+# plot(crops)
+# plot(aoi, add=T)
 
-crop2[crop2$`mnc-verano-2021`==22] <- 0
-crop2[crop2$`mnc-verano-2021`!=0] <- 1
-plot(crop2)
-crops <- crop1+crop2
-crops[crops$`mnc-invierno-2020`==2] <- 1
-names(crops) <- "crops"
-plot(crops)
-plot(aoi, add=T)
+# writeRaster(crops, "01-Data/land cover/croplands.tif")
+names(aoi)[5] <- "stratum"
 
-writeRaster(crops, "01-Data/land cover/croplands.tif")
-aoi$COD_DEPTO_ <- as.numeric(aoi$COD_DEPTO_)
-names(aoi)[5] <- "district"
-
-x$district <- as.numeric(x$district)
-
-aoir <- rasterize(aoi, crops, field = "district")
+crops <- rast("01-Data/land cover/croplands.tif")
+aoir <- rasterize(aoi, crops, field = "stratum")
 plot(aoir)
 aoir_crop <- aoir*crops
-aoir_crop[aoir_crop$district==0] <- NaN
+aoir_crop[aoir_crop$stratum==0] <- NaN
 plot(aoir_crop)
-writeRaster(aoir_crop, "01-Data/land cover/districts_croplands.tif")
-
+aoir_crop <- terra::trim(aoir_crop)
+crops <- crop(crops, aoir_crop)
+writeRaster(aoir_crop, "01-Data/land cover/SE_districts_croplands.tif", overwrite=TRUE)
+writeRaster(crops, "01-Data/land cover/SE_croplands.tif")
+# 
 
--- 03-Scripts/4.1.Quantile_Regression_Forest_no_coord.R
+++ 03-Scripts/4.1.Quantile_Regression_Forest_no_coord.R
@@ -26,14 +26,13 @@ gc()
 # 0 - Set working directory, soil attribute, and packages ======================
 
 # Working directory
-wd <- 'C:/GIT/Digital-Soil-Mapping'
-setwd(wd)
+setwd('C:/GIT/Digital-Soil-Mapping')
 
 # Load Area of interest (shp)
 AOI <- '01-Data/buenos_aires.shp'
 
 # Terget soil attribute
-soilatt <- 'p'
+soilatt <- 'p_bray'
 
 # Function for Uncertainty Assessment
 load(file = "03-Scripts/eval.RData")
@@ -46,54 +45,353 @@ library(quantregForest)
 library(terra)
 library(sf)
 library(doParallel)
-
+library(mapview)
+# install.packages("terra", repos = "https://rspatial.r-universe.dev")
 
 # 1 - Merge soil data with environmental covariates ============================
 
-stratum <- rast("01-Data/land cover/districts_croplands.tif")
-names(stratum) <- "stratum"
-y <- freq(stratum)
-y <- y$value[y$count<300]
-stratum[stratum$stratum %in% y] <- NaN
-x <- spatSample(x = stratum, 200, method = "stratified", as.df=T, xy=T, replace=F)
-table(x$stratum)
+val_data <- read_csv("01-Data/data_with_coord.csv") %>% 
+  vect(geom=c("x", "y"), crs = "epsg:4326")
 
-plot(stratum)
-dat <- read_csv("01-Data/sim_data_no_coord.csv")
-names(dat)[1] <- "stratum"
+covs <- rast("01-Data/covs/covs.tif")
+ncovs <- names(covs)
 
-dat$stratum <- as.numeric(dat$stratum)
-
-unique(x$stratum)
+ex <- terra::extract(x = covs, y = val_data, xy=F)
+val_data <- as.data.frame(val_data)
+val_data <- cbind(val_data, ex)
+val_data <- na.omit(val_data)
 
 ## 1.1 - Load covariates -------------------------------------------------------
-files <- list.files(path= '01-Data/covs/', pattern = '.tif$', full.names = T)
-covs <- rast(files)
-ncovs <- str_remove(files, "01-Data/covs/")
-ncovs <- str_remove(ncovs, ".tif")
-ncovs <- str_replace(ncovs, "-", "_")
-names(covs) <- ncovs 
+# files <- list.files(path= '01-Data/covs/', pattern = '.tif$', full.names = T)
+# covs <- rast(files)
+# ncovs <- str_remove(files, "01-Data/covs/")
+# ncovs <- str_remove(ncovs, ".tif")
+# ncovs <- str_replace(ncovs, "-", "_")
+# names(covs) <- ncovs 
+
+# aoi <- vect("01-Data/AOI_Arg.shp")
+# mask <- rasterize(aoi, covs[[1]])
+# mask <- trim(mask)
+# covs <- crop(covs, mask) %>% mask(mask)
+# plot(covs)
+# writeRaster(covs, "01-Data/covs/covs.tif")
+
+
 
 ## 1.2 - Load the soil data (Script 2) -----------------------------------------
-dat <- read_csv("02-Outputs/soil_data.csv")
+# dat <- read_csv("02-Outputs/soil_data.csv")
 
 # Convert soil data into a spatial object (check https://epsg.io/6204)
-dat <- vect(dat, geom=c("x", "y"), crs = "epsg:6204")
 
-# Reproject point coordinates to match coordinate system of covariates
-dat <- terra::project(dat, covs)
-names(dat)
+df <- NULL
+dat <- read_csv("01-Data/sim_data_no_coord.csv")
+N <- dat %>% group_by(stratum) %>% summarise(N=n())
 
-## 1.3 - Extract values from covariates to the soil points ---------------------
-pv <- terra::extract(x = covs, y = dat, xy=F)
-dat <- cbind(dat,pv)
-dat <- as.data.frame(dat)
+for (i in 1:50) {
+  stratum <- rast("01-Data/land cover/SE_districts_croplands.tif")
+  names(stratum) <- "stratum"
+  
+  y <- freq(stratum)
+  
+  x <- spatSample(x = stratum, max(N$N), method = "stratified", as.df=T, xy=T, replace=F)
+  table(x$stratum)
+  
+  dat <- read_csv("01-Data/sim_data_no_coord.csv")
+  
+  strata <- unique(x$stratum)
+  d <- NULL
+  for (j in seq_along(strata)) {
+    z <- x[x$stratum==strata[j],]
+    n <- N[N$stratum==strata[j],"N"][[1]]
+    z <- z[sample(1:max(N$N), size = n),]
+    if(strata[j] %in% dat$stratum){
+      z <- cbind(z,dat[dat$stratum==strata[j],soilatt])
+      d <- rbind(d,z)}
+  }
+  
+  # d %>% 
+  #   st_as_sf(coords = c("x", "y"), crs = 4326) %>% # convert to spatial object
+  #   mapview(zcol = "p", cex = 2, lwd = 0.1) 
+  dat <- vect(d, geom=c("x", "y"), crs = "epsg:4326")
+  
+  # Reproject point coordinates to match coordinate system of covariates
+  # dat <- terra::project(dat, covs)
+  # names(dat)
+  
+  ## 1.3 - Extract values from covariates to the soil points ---------------------
+  pv <- terra::extract(x = covs, y = dat, xy=F)
+  dat <- cbind(dat,pv)
+  dat <- as.data.frame(dat)
+  
+  
+  
+  ## 1.4 - Target soil attribute + covariates ------------------------------------
+  d <- select(dat, soilatt, names(covs))
+  d <- na.omit(d)
+  
+  
+  # 2 - Covariate selection with RFE =============================================
+  ## 2.1 - Setting parameters ----------------------------------------------------
+  # Repeatedcv = 3-times repeated 10-fold cross-validation
+  # fitControl <- rfeControl(functions = rfFuncs,
+  #                          method = "cv",
+  #                          number = 2,         ## 10 -fold CV
+  #                          # repeats = 2,        ## repeated 3 times
+  #                          verbose = TRUE,
+  #                          saveDetails = TRUE, 
+  #                          returnResamp = "all")
+  
+  # Set the regression function
+  fm = as.formula(paste(soilatt," ~", paste0(ncovs,
+                                             collapse = "+")))
+  
+  # Calibrate the model using multiple cores
+  # cl <- makeCluster(detectCores()-1)
+  # registerDoParallel(cl)
+  
+  
+  ## 2.2 - Calibrate a RFE model to select covariates ----------------------------
+  # covsel <- rfe(fm,
+  #               data = d,  
+  #               sizes = seq(from=10, to=length(ncovs)-1, by = 10),
+  #               rfeControl = fitControl,
+  #               verbose = TRUE,
+  #               keep.inbag = T)
+  # stopCluster(cl)
+  # 
+  # ## 2.3 - Plot selection of covariates ------------------------------------------
+  # trellis.par.set(caretTheme())
+  # plot(covsel, type = c("g", "o"))
+  # 
+  # # Extract selection of covariates and subset covs
+  # opt_covs <- predictors(covsel)[1:10]
+  
+  # 3 - QRF Model calibration ====================================================
+  ## 3.1 - Update formula with the selected covariates ---------------------------
+  # fm <- as.formula(paste(soilatt," ~", paste0(opt_covs, collapse = "+")))
+  
+  # parallel processing
+  print(paste("start fitting model", i))
+  timestamp()
+  # cl <- makeCluster(detectCores()-1)
+  # registerDoParallel(cl)
+  
+  ## 3.2 - Set training parameters -----------------------------------------------
+  fitControl <- trainControl(method = "none",
+                             # number = 10,         ## 10 -fold CV
+                             # repeats = 3,        ## repeated 3 times
+                             savePredictions = TRUE)
+  
+  # Tune mtry hyperparameters
+  mtry <- round(length(ncovs)/3)
+  tuneGrid <-  expand.grid(mtry = c(mtry))
+  
+  ## 3.3 - Calibrate the QRF model -----------------------------------------------
+  model <- caret::train(fm,
+                        data = d,
+                        method = "qrf",
+                        trControl = fitControl,
+                        verbose = TRUE,
+                        tuneGrid = tuneGrid,
+                        keep.inbag = T,
+                        importance = TRUE, )
+  # stopCluster(cl)
+  # gc()
+  print(paste("end fitting", i))
+  timestamp()
+  
+  ## 3.4 - Extract predictor importance as relative values (%)
+  x <- randomForest::importance(model$finalModel)
+  model$importance <- x
+  ## 3.5 - Print and save model --------------------------------------------------
+  print(model)
+  saveRDS(model, file = paste0("02-Outputs/models/model_",soilatt,"_",i,".rds"))
+  
+  # 4 - Uncertainty assessment ===================================================
+  # extract observed and predicted values
+  o <- val_data$p
+  p <- predict(model$finalModel, val_data, what = mean)
+  df <- rbind(df, data.frame(o,p, iteration=i))
+  
+  ## 4.1 - Plot and save scatterplot --------------------------------------------- 
+  (g1 <- ggplot(df, aes(x = o, y = p)) + 
+     geom_point(alpha = 0.5) + 
+     geom_abline(slope = 1, intercept = 0, color = "red")+
+     ylim(c(min(o), max(o))) + theme(aspect.ratio=1)+ 
+     labs(title = soilatt) + 
+     xlab("Observed") + ylab("Predicted"))
+  ggsave(g1, filename = paste0("02-Outputs/residuals_",soilatt,"_",i,".png"), 
+         scale = 1,
+         units = "cm", width = 12, height = 12)
+  
+  ## 4.2 - Print accuracy coeficients --------------------------------------------
+  # https://github.com/AlexandreWadoux/MapQualityEvaluation
+  print(paste("model", i))
+  print(eval(p,o))
+  
+  ## 4.3 - Plot Covariate importance ---------------------------------------------
+  (g2 <- varImpPlot(model$finalModel, main = soilatt, type = 1))
+  
+  png(filename = paste0("02-Outputs/importance_",soilatt,"_",i,".png"), 
+      width = 15, height = 15, units = "cm", res = 600)
+  g2
+  dev.off()
+}
+write_csv(df, paste("residuals_",soilatt,".csv"))
+# 5 - Prediction ===============================================================
+# Generation of maps (prediction of soil attributes) 
+## 5.1 - Produce tiles ---------------------------------------------------------
+r <-covs[[1]]
+t <- rast(nrows = 5, ncols = 5, extent = ext(r), crs = crs(r))
+tile <- makeTiles(r, t,overwrite=TRUE,filename="02-Outputs/tiles/tiles.tif")
 
-summary(dat)
+## 5.2 - Predict soil attributes per tiles -------------------------------------
+# loop to predict on each tile
+
+for (j in seq_along(tile)) {
+  for (i in 1:50) {
+    
+    gc()
+    t <- rast(tile[j])
+    covst <- crop(covs, t)
+    
+    model <- readRDS(paste0("02-Outputs/models/model_",soilatt,"_", i,".rds"))
+    # plot(r)# 
+    meanFunc = function(x) mean(x, na.rm=TRUE)
+    pred_mean <- terra::predict(covst, model = model$finalModel, na.rm = TRUE,   
+                                cpkgs="quantregForest", what=meanFunc)
+    sdFunc = function(x) sd(x, na.rm=TRUE)
+    pred_sd <- terra::predict(covst, model = model$finalModel, na.rm=TRUE,  
+                              cpkgs="quantregForest", what=sdFunc)  
+    
+    writeRaster(pred_mean, 
+                filename = paste0("02-Outputs/tiles/soilatt_tiles/",
+                                  soilatt,"_tile_", j,"_model_",i,".tif"), 
+                overwrite = TRUE)
+    writeRaster(pred_sd, 
+                filename = paste0("02-Outputs/tiles/soilatt_tiles/",
+                                  soilatt,"_tileSD_", j,"_model_",i,".tif"), 
+                overwrite = TRUE)
+    
+    rm(pred_mean)
+    rm(pred_sd)
+    
+    
+    print(paste("tile",tile[j],"model", i))
+  }
+}
+
+## 5.3 - Merge tiles both prediction and st.Dev --------------------------------
+for (j in 1:25) {
+  f_mean <- list.files(path = "02-Outputs/tiles/soilatt_tiles/", 
+                       pattern = paste0(soilatt,"_tile_",j,"_model_"), full.names = TRUE)
+  f_sd <- list.files(path = "02-Outputs/tiles/soilatt_tiles/", 
+                     pattern =  paste0(soilatt,"_tileSD_",j,"_model_"), full.names = TRUE)
+  pred_mean <- rast(f_mean) %>% mean(na.rm=TRUE)
+  # q05 <- rast(f_mean) %>% quantile(.05)
+  # q95 <- rast(f_mean) %>% quantile(.95)
+  r_sd_l <- rast(f_sd) %>% mean(na.rm=TRUE)
+  names(r_sd_l) <- "sd"
+  
+  writeRaster(r_sd_l, 
+              filename = paste0("02-Outputs/tiles/aggregated_tiles/",
+                                soilatt,"_tilesd_", j,".tif"), 
+              overwrite = TRUE)
+  writeRaster(q05, 
+              filename = paste0("02-Outputs/tiles/aggregated_tiles/",
+                                soilatt,"_tileq05_", j,".tif"), 
+              overwrite = TRUE)
+  writeRaster(q50, 
+              filename = paste0("02-Outputs/tiles/aggregated_tiles/",
+                                soilatt,"_tileq50_", j,".tif"), 
+              overwrite = TRUE)
+  writeRaster(q95, 
+              filename = paste0("02-Outputs/tiles/aggregated_tiles/",
+                                soilatt,"_tileq95_", j,".tif"), 
+              overwrite = TRUE)
+}
+
+name_tiles <- c("sd_", "q05_","q50_", "q95_")
+
+for (i in seq_along(name_tiles)) {
+  f <- list.files(path = "02-Outputs/tiles/aggregated_tiles/", 
+                  pattern = name_tiles[i], full.names = TRUE)
+  r <- list()
+  for (g in seq_along(f)){
+    r[[g]] <- rast(f[g])
+    print(g)
+  }
+  r <- sprc(r)
+  r <- mosaic(r)
+  plot(r)
+  writeRaster(r, paste0("02-Outputs/maps/",soilatt,name_tiles[i],".tif"),
+              overwrite=TRUE)
+  rm(r)
+  gc()
+}
+
+val_data <- read_csv("01-Data/data_with_coord.csv") %>% 
+  vect(geom=c("x", "y"), crs = "epsg:4326")
+
+q50 <- rast("02-Outputs/maps/p_tileq50.tif")
+
+ex <- terra::extract(x = q50, y = val_data, xy=F, ID=FALSE)
+val_data <- as.data.frame(val_data)
+val_data <- cbind(val_data, ex)
+val_data <- na.omit(val_data)
+
+eval(val_data$q0.50,val_data$p)
+
+##############################
+
+v <- st_read("01-Data/AOI_Arg.shp")
+names(v)[5] <- "stratum"
+darea <- readxl::read_excel("01-Data/data_without_coord.xls")
+darea <-  darea %>% 
+  select(stratum=code, median)
+darea$stratum <- as.numeric(darea$stratum)
+v <- left_join(v, darea)
+v <- vect(v)
+names(v)[8] <- "p"
+plot(v, "p")
+r <- rast("01-Data/land cover/SE_croplands.tif")
+r <- rasterize(v,r,field="p")
+plot(r)
+val_data <- read_csv("01-Data/data_with_coord.csv") %>% 
+  vect(geom=c("x", "y"), crs = "epsg:4326")
+
+val_data$pred <- terra::extract(r, val_data, ID=FALSE)[[1]]
+
+val <- val_data %>% as.data.frame() %>% na.omit()
+
+eval(val$pred, val$p)
+
+##################
+
+
+val_data <- read_csv("01-Data/data_with_coord.csv") %>% 
+  vect(geom=c("x", "y"), crs = "epsg:4326")
+
+covs <- rast("01-Data/covs/covs.tif")
+psd <- rast("02-Outputs/maps/p_tileSD.tif")
+p05 <- rast("02-Outputs/maps/p_tileq05.tif")
+p50 <- rast("02-Outputs/maps/p_tileq50.tif")
+p95 <- rast("02-Outputs/maps/p_tileq95.tif")
+covs <- c(covs, psd, p05, p50, p95)
+
+ncovs <- names(covs)
+
+ex <- terra::extract(x = covs, y = val_data, xy=F)
+val_data <- as.data.frame(val_data)
+val_data <- cbind(val_data, ex)
+val_data <- na.omit(val_data)
+
+plot(covs[[71]])
 
 ## 1.4 - Target soil attribute + covariates ------------------------------------
-d <- select(dat, soilatt, names(covs))
+d <- select(val_data, soilatt, ncovs)
 d <- na.omit(d)
+
 
 # 2 - Covariate selection with RFE =============================================
 ## 2.1 - Setting parameters ----------------------------------------------------
@@ -108,7 +406,7 @@ fitControl <- rfeControl(functions = rfFuncs,
 
 # Set the regression function
 fm = as.formula(paste(soilatt," ~", paste0(ncovs,
-                                             collapse = "+")))
+                                           collapse = "+")))
 
 # Calibrate the model using multiple cores
 cl <- makeCluster(detectCores()-1)
@@ -129,7 +427,7 @@ trellis.par.set(caretTheme())
 plot(covsel, type = c("g", "o"))
 
 # Extract selection of covariates and subset covs
-opt_covs <- predictors(covsel)[1:55]
+opt_covs <- predictors(covsel)[1:65]
 
 # 3 - QRF Model calibration ====================================================
 ## 3.1 - Update formula with the selected covariates ---------------------------
@@ -177,17 +475,17 @@ df <- data.frame(o,p)
 
 ## 4.1 - Plot and save scatterplot --------------------------------------------- 
 (g1 <- ggplot(df, aes(x = o, y = p)) + 
-  geom_point(alpha = 0.1) + 
+   geom_point(alpha = 0.3) + 
    geom_abline(slope = 1, intercept = 0, color = "red")+
-  ylim(c(min(o), max(o))) + theme(aspect.ratio=1)+ 
-  labs(title = soilatt) + 
-  xlab("Observed") + ylab("Predicted"))
+   ylim(c(min(o), max(o))) + theme(aspect.ratio=1)+ 
+   labs(title = soilatt) + 
+   xlab("Observed") + ylab("Predicted"))
 ggsave(g1, filename = paste0("02-Outputs/residuals_",soilatt,".png"), scale = 1, 
        units = "cm", width = 12, height = 12)
 
 ## 4.2 - Print accuracy coeficients --------------------------------------------
 # https://github.com/AlexandreWadoux/MapQualityEvaluation
-eval(o,p)
+eval(p,o)
 
 ## 4.3 - Plot Covariate importance ---------------------------------------------
 (g2 <- varImpPlot(model$finalModel, main = soilatt, type = 1))
@@ -252,8 +550,10 @@ for (j in seq_along(tile)) {
 ## 5.3 - Merge tiles both prediction and st.Dev --------------------------------
 f_mean <- list.files(path = "02-Outputs/tiles/soilatt_tiles/", 
                      pattern = paste0(soilatt,"_tile_"), full.names = TRUE)
+f_mean <- f_mean[str_detect(f_mean, "model", negate = TRUE)]
 f_sd <- list.files(path = "02-Outputs/tiles/soilatt_tiles/", 
                    pattern =  paste0(soilatt,"_tileSD_"), full.names = TRUE)
+f_sd <- f_sd[str_detect(f_sd, "model", negate = TRUE)]
 r_mean_l <- list()
 r_sd_l <- list()
 
@@ -285,14 +585,9 @@ plot(pred_sd)
 
 # 6 - Export final maps ========================================================
 writeRaster(pred_mean, 
-            paste0("02-Outputs/maps/",soilatt,"_QRF.tif"),
+            paste0("02-Outputs/maps/",soilatt,"_xy_QRF.tif"),
             overwrite=TRUE)
 writeRaster(pred_sd, 
-            paste0("02-Outputs/maps/",soilatt,"_QRF_SD.tif"),
+            paste0("02-Outputs/maps/",soilatt,"_xy_QRF_SD.tif"),
             overwrite=TRUE)
-
-
-
-
-
 
